bc17da35e63a4b7f207a54a404a676ee5f250bf5
Alina Pan <alinapanyue@outlook.com>
2024-08-29 23:27:23 -0400

    updated the page by adding pictures, a return button, a refresh button, and other related features

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
+const clcBtn = document.getElementById("calc-btn");
+const refreshBtn = document.getElementById("refresh-btn");
+const returnBtn = document.getElementById("return-btn");
 const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
-function calcWakeUpTimes(){
-    wakeUpHoursDiv.innerHTML = "";
-    const fallAsleepTime = new Date();
-    fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
-    
-    //const wakeUpTimesDiv = document.getElementById("wakeup-hours-div");
-    //wakeUpTimesDiv.innerHTML = "";
-    const wakeUpTime = new Date(fallAsleepTime);
-    //const wakeUpTimes = [];
-    for (let i = 1; i <= 6; i++) {
-      wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
-      const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
-        timeStyle: "short",
-      });
-      const cycleDiv = document.createElement('div');
-      cycleDiv.classList.add("cycle");
-      cycleDiv.setAttribute("id", `cycle-${i}`);
-      cycleDiv.textContent = wakeUpTimeString;
-      wakeUpHoursDiv.appendChild(cycleDiv);
-      //wakeUpTimes.push(wakeUpTimeString);
+const promptSection = document.getElementById("prompt-section");
+const imageContainer = document.getElementById("img-container");
+const resultSection = document.getElementById("result-section");
 
-    }
-    
-    //console.log("Wake-up times:", wakeUpTimes.join(", "));
+clcBtn.addEventListener("click", calcWakeUpTimes);
+refreshBtn.addEventListener("click", calcWakeUpTimes);
+returnBtn.addEventListener("click", () => {
+  promptSection.classList.remove("hidden");
+  imageContainer.classList.remove("hidden");
+  resultSection.classList.add("hidden");
+});
+
+function calcWakeUpTimes() {
+  const fallAsleepTime = new Date();
+  fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+
+  const wakeUpTime = new Date(fallAsleepTime);
+  wakeUpHoursDiv.innerHTML = "";
+  for (let i = 1; i <= 6; i++) {
+    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+    const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
+      timeStyle: "short",
+    });
+    const cycleDiv = document.createElement("div");
+    cycleDiv.classList.add("cycle");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = wakeUpTimeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
+  }
+
+  promptSection.classList.add("hidden");
+  imageContainer.classList.add("hidden");
+  resultSection.classList.remove("hidden");
 }
-const calcBtn = document.getElementById("calc-btn");
-calcBtn.onclick = calcWakeUpTimes;//no parenthesis not calling, but assigning
